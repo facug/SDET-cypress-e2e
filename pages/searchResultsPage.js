b7d4a8cbff1941ea4d0240c9f93e84dfa3eb27e2
unknown <facundogmonzon@gmail.com>
2021-03-21 22:05:39 -0300

    adding goToPage function to use pagination

--- pages/searchResultsPage.js
+++ pages/searchResultsPage.js
@@ -1,8 +1,10 @@
 let searchResultsPage = function () {
 
-    this.goToPage= function(pageNumber) {
-        $(".next-pagination-list button:nth-child({pageNumber})").click();
+    this.goToPage= async function(pageNumber) {
+        await browser.executeScript('arguments[0].scrollIntoView()', $('.teletext-subject'));
+        browser.sleep(500)
+        await browser.executeScript("window.scrollBy(0,-250)");
+        $(`.next-pagination-list button:nth-child(${pageNumber})`).click();
     }
-
 }
 module.exports = new searchResultsPage();
--- todo-spec.js
+++ todo-spec.js
@@ -5,15 +5,11 @@ describe('searching for products in Aliexpress', function() {
   it('search for iphone product and check the 2nd page', async function() {
     browser.driver.get('https://www.aliexpress.com ');
 
-    //const couponModal = 'img.btn-close';
-
-    //browser.driver.findElement(by.css(couponModal)).click();
-
     aliexpressHomePage.closeModals();
-    //browser.driver.findElement(by.css('.search-key-box input')).sendKeys('iphone');
     aliexpressHomePage.searchForProduct("iphone");
+    await searchResultsPage.goToPage(2);
     await sleep(10000);
-    console.log('Two seconds later, showing sleep in a loop...');
+    console.log('waiting for a bit...');
 
   });
 });
