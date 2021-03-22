cc686d2242cdb023867b6412a2127dab81f575e6
unknown <facundogmonzon@gmail.com>
2021-03-21 21:19:13 -0300

    renaming searchResultsPage class

--- todo-spec.js
+++ todo-spec.js
@@ -1,5 +1,5 @@
 let aliexpressHomePage = require('./pages/aliexpressHomePage')
-let searchResultsPage = require('./pages/searchResultspage')
+let searchResultsPage = require('./pages/searchResultsPage')
 
 describe('searching for products in Aliexpress', function() {
   it('search for iphone product and check the 2nd page', async function() {
